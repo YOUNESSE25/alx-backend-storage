c6f8b42ce1425127c09cab809925cfe3a7b3ed03
YOUNESSE25 <younessehaouas@gmail.com>
2024-07-17 20:14:09 +0100

    redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,25 +16,23 @@ def count_url_access(method):
     """
     @wraps(method)
     def wrapper(url):
-        CachedKey = "cached:" + url
-        CachedData = store.get(CachedKey)
-        if CachedData:
-            return CachedData.decode("utf-8")
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-        countKey = "count:" + url
+        count_key = "count:" + url
         html = method(url)
 
-        store.incr(countKey)
-        store.set(CachedKey, html)
-        store.expire(CachedKey, 10)
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
         return html
     return wrapper
 
 
 @count_url_access
 def get_page(url: str) -> str:
-    """
-    returns HTML content of a url
-    """
+    """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
