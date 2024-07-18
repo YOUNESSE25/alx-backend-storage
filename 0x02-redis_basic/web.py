56dbc9b9b8a4e5e5e4d74bf60fd9c07266262efd
YOUNESSE25 <younessehaouas@gmail.com>
2024-07-18 01:48:50 +0100

    redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
 '''
+ Implementing an expiring web cache and tracker
+ '''
 import redis
 import requests
 from functools import wraps
@@ -8,31 +9,31 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
+    '''
+    Caches data.
     '''
     @wraps(method)
     def invoker(url) -> str:
-        '''The wrapper function for caching the output.
+        '''
+        caching the output.
         '''
         redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
+        res = redis_store.get(f'result:{url}')
+        if res:
+            return res.decode('utf-8')
+        res = method(url)
         redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
+        redis_store.setex(f'result:{url}', 10, res)
+        return res
     return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
+    '''
+    returns the content of a URL
     '''
     return requests.get(url).text
